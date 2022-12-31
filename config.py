9314ecac7fbb3077d466944d87013c120423606f
stefan.franz <36929312+stefran65@users.noreply.github.com>
2022-12-31 12:53:51 +0100

    Update config.py
    Added
    SQLALCHEMY_TRACK_MODIFICATIONS

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
-
 # DONE IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/fyyur'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
